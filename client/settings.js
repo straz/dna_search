c0d9ed885592db32226ed3dee07638e7b976ea38
Steve Strassmann <steve@strassmann.com>
2019-06-26 00:38:33 -0400

    prd not dev

--- client/settings.js
+++ client/settings.js
@@ -1,6 +1,6 @@
 const SETTINGS = {
   "env": "prd",
   "email": "guest@example.com",
-  "api_url": "http://127.0.0.1:3000",
+  "api_url": "https://wxt5vzyewl.execute-api.us-east-1.amazonaws.com/prd",
   "upload_url": "https://ginkgo-search.s3.amazonaws.com"
 }
